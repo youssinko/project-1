c393d7b8a532f07205943ab8e0c6ec26aab0b5fe
Rania Elnaggar <yousef_amir1@hotmail.com>
2022-11-06 15:59:24 -0500

    fixed timer

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const secondSymbol = document.querySelector("#secondSymbol")
 const timer = document.querySelector("#time")
 const notClicked = document.querySelector('.notclicked')
 let timeSecond = 10;
-timer.innerHTML=`00:${timeSecond}`
+
 
 const winConditions = [
     [0, 1, 2],
@@ -32,7 +32,10 @@ const winConditions = [
 
 ]
 
-let options = ['', '', '', '', '', '', '', '', '']
+let options = [
+    '', '', '',
+    '', '', '',
+    '', '', '']
 let currentPlayer = "X"
 
 let gameIsRunning = false;
@@ -40,7 +43,7 @@ let gameIsRunning = false;
 //ENTER BUTTON FUNCTION : Player 1 get to choose symbol and input name, then input field disappears and new input for player 2 appears *
 // 2 "enter" buttons were created for each player and one play button to start the game
 
-//====================== first enter button function ======================
+//====================== first 'enter' button function ======================
 function enter() {
 
     displayName.innerHTML = `Player 1 : ${p1Name.value.toUpperCase()} <br> Symbol : ${selectionEl.value}`;//
@@ -54,7 +57,7 @@ function enter() {
 
 enterBtn.addEventListener('click', enter)
 
-//========================= second enter button function ================
+//========================= second 'enter' button function ================
 
 function enter2() {
 
@@ -63,19 +66,35 @@ function enter2() {
     player2Container.classList.toggle('hide')
     playBtn.classList.remove('hide');
     gif.classList.add('hide')
-    
+
 
 }
 
 enter2Btn.addEventListener('click', enter2)
-//====================== Play button ===========
+//====================================== 'Play' button =======================================
 function play() {
     gameEl.classList.remove('hide')
     // document.querySelector('body').style.background = "URL(./images/stick-figure.gif)";
     playBtn.classList.add('hide')
     setTimer()
+
 }
 playBtn.addEventListener('click', play)
+//========================== 'restart' button function to empty cells for the next round ========
+function restartGame() {
+    options = [
+        '', '', '',
+        '', '', '',
+        '', '', '']
+    currentPlayer = "X";
+    statusText.textContent = `${currentPlayer}'s turn`
+    cells.forEach(cell =>
+        cell.textContent = '')
+    gameIsRunning = true
+    document.querySelector('body').style.background = ""
+    timeSecond = 10
+
+}
 //========================= create function when player 1 select symbol, player 2 assigned automatically to the second symbol =========
 let selectedSymbol = ''
 function disableSymbol(disableOption) {
@@ -100,36 +119,39 @@ function startGame() {
         restartBtn.addEventListener('click', restartGame);
         statusText.textContent = `${currentPlayer}'s turn`;
         gameIsRunning = true
-        
+
+
     })
-    
+
 }
 
 //function where we can get attribute of each cell to check if cell is empty and only update cell if it's empty
 
 function cellclicked() {
-    // let timeSecond = 10;
-    // setTimer()
+
     const cellIndex = this.getAttribute('cellIndex')
 
     //if cell is empty or game is not running, don't do anything
     //
     if (options[cellIndex] !== '' || !gameIsRunning) {
+
         return
     }
     else {
-        //cells.classList.toggle('notclicked')
+
         updatecell(this, cellIndex)
         winner()
-        
+
+
     }
-    
+
 }
 //function to updatecell  with the currentPlayer when clicked
 function updatecell(cell, index) {
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
-    
+    timeSecond = 10
+
 }
 
 //function to switch players if below conditions are false
@@ -137,11 +159,14 @@ function switchPlayer() {
     if (currentPlayer == 'X') {
         currentPlayer = 'O'
         statusText.textContent = `${currentPlayer}'s Turn`
-        
+
+
+
     } else {
         currentPlayer = 'X'
         statusText.textContent = `${currentPlayer}'s Turn`
-        
+
+
     }
 
 }
@@ -161,97 +186,88 @@ function winner() {
         }
         if (cellA == cellB && cellB == cellC) {
             roundWon = true;
+
             break;
         }
     }
-    //outside for loop
+    //outside 'for loop'
     if (roundWon === true) {
         document.querySelector('body').style.backgroundImage = "URL(./images/img1.gif)"
-        statusText.textContent = `${currentPlayer} wins!` 
+        statusText.textContent = `${currentPlayer} wins!`
         gameIsRunning = false
        
-        
-        
+
+
+
+
     }
     else if (!options.includes('')) {
         statusText.textContent = 'Draw!'
         gameIsRunning = false;
         restartBtn.addEventListener('click', restartGame);
-        
+        //clearInterval(countdown)
     }
-    else{
+    else {
         switchPlayer()
     }
 }
 
 //coundown function
-function setTimer(){
-    
-    countdown = setInterval(()=>{
-   timeSecond--;
-   timer.innerHTML=`00:0${timeSecond}`;
-   if(timeSecond <=0 || timeSecond < 1){
-       endtime()
-       clearInterval(countdown)
-       
-       randomPlay()
-      
-   }else if(gameIsRunning == false){
-    clearInterval(countdown)
-    
-   }
-},1000)
 
+function setTimer() {
 
-}
-///function to statement notifying time is out
-function endtime(){
-   timer.innerHTML = `TIME OUT ... Computer will Play for You`
-}
+    countdown = setInterval(() => {
+        timeSecond--;
+        timer.innerHTML = `00:0${timeSecond}`;
 
-//restart button function to empty cells for the next round
-function restartGame() {
-   options = ['', '', '', '', '', '', '', '', '']
-    currentPlayer = "X";
-    statusText.textContent = `${currentPlayer}'s turn`
-cells.forEach(cell => 
-    cell.textContent = '')
-    gameIsRunning = true
-    document.querySelector('body').style.background = ""
-    //clearInterval(countdown)
-     //setTimer()
-   
-}
-//function where when time is out computer will randomly plays for u
-//let computerMoves = []
-function randomCell(){
-    random = Math.floor(Math.random() * (cells.length) )
-    return random
-}
-  function randomPlay(){
-  
-       
-        let random = randomCell()
-        if(cells[random].textContent !== ""){
-            randomPlay() 
+        if (timeSecond <= 0 || timeSecond < 1) {
+            endtime()
+            // clearInterval(countdown)
+            timeSecond = 10
+            randomPlay()
+
         }
-       else{
-        cells[random].textContent= currentPlayer
-        cells.textContent = currentPlayer
-        switchPlayer()
-      }     
-        
+        else if (gameIsRunning === false) {
+
+            clearInterval(countdown)
+
+        }
+
+    }, 1000)
+}
+
+
+
+///function to statement notifying time is out
+function endtime() {
+    timer.innerHTML = `TIME OUT ... Computer will Play for You`
+}
+
+
+//=========================== function when time is out, computer will randomly plays for you ============
+
+function randomCell() {
+
+    random = Math.floor(Math.random() * (options.length))
+    return random
+
+}
+function randomPlay() {
+
+
+    let random = randomCell()
+    if (cells[random].textContent !== "") {
+        randomPlay()
+
     }
 
+    else {
+        cells[random].textContent = currentPlayer
+        cells.textContent = currentPlayer
+        winner()
 
-    // options[random]= currentPlayer
-    // //console.log(random)
-  
-    // cells.textContent = currentPlayer
-    // console.log(currentPlayer)
-   //if (options[random] !== '' || !gameIsRunning)
-   //}
-// //need to pick a random index and if cell is empty, computer can place symbol
-
- //randomPlay()
-//setTimer()
+    }
+    // if (!cells.textContent.includes('')){
+    //     gameIsRunning = false
+    // }
+}
